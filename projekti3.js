f2475869e22689b593d5a6a9e40522b11d7ee967
ssirina <54996635+ssirina@users.noreply.github.com>
2021-12-02 10:50:34 +0200

    Added smoother transitions

--- projekti3.js
+++ projekti3.js
@@ -2,13 +2,37 @@
 $("#hakuBtn").click(function() {
     haeKaikki();
 });
+
+//dokumentin latauduttua suoreitettavat komennot
 $(document).ready(function() {
+    //haetaan etusivulle kuva
     haeKuva();
+    //piilotetaan otsikko ja feidataan se sisään
+    $("h1").hide();
+    $("h1").html("Cat Fact Generator").fadeIn(500);
+    //animoidaan logo tärähtämään
+    $("#logo").animate(
+        {
+            width: "100px",
+        }, 200
+    ).animate(
+        {
+            width: "40px",
+        }, 100
+    ).animate(
+        {
+                width: "90px",
+        }, 100
+    ).animate(
+            {
+                width: "70px",
+            }
+    );
 })
 
 //FUNKTIOT
 
-//Haku-nappia painettaessa haetaan kuva ja fakta, sekä piilotetaan sivulta yksi elementti (mikäli vielä esillä)
+//Haku-nappia painettaessa haetaan kuva ja fakta, sekä piilotetaan sivulta yksi elementti (mikäli vielä esillä) ja heilautetaan logoa
 function haeKaikki() {
     haeKuva();
     haeFakta();
@@ -40,81 +64,64 @@ function piilota() {
 
 // Haetaan kuvan url API:sta
 function haeKuva() {
-    //console.log("haeKuva()")
-    var request = new XMLHttpRequest;
-    var url = "https://api.thecatapi.com/v1/images/search";
-
-    request.open("GET", url, true);
-    request.send();
-
-    request.onreadystatechange = function() {
-        if(request.readyState == 4 && request.status == 200) {
-            //console.log(request.responseText);
-            parseKuva(request.responseText);
-        } else if(request.readyState == 4 && request.status != 200) {
-            //Jos haku ei onnistu (haku on valmis, mutta status ei ole "OK"), kerrotaan käyttäjälle ja pyydetään suorittamaan uusi haku:
-            alert("Something went wrong while fetching a cat picture. Please try again!")
+    $("#image").hide(300);
+    $.ajax({
+        url: "https://api.thecatapi.com/v1/images/search",
+        //jos haku onnistuu:
+        success: function(result) {
+            //console.log(result[0].url);
+            naytaKuva(result[0].url);
+        },
+        // jos haku ei onnistu:
+        error: function(errorThrown) {
+            alert("There was a problem while fetching a cat image. Please try again. Error: " + errorThrown);
+            console.log(errorThrown);
         }
-    }
+    });
 }
 
-//parsitaan haun response text ("kuva") ja valitaan json-muotoisesta objektista haluttu tieto (=url)
-function parseKuva(kuva) {
-    var jsonObjekti = JSON.parse(kuva);
-    //console.log(jsonObjekti);
-    var kuva = jsonObjekti[0].url;
-    //console.log(kuva);
-    naytaKuva(kuva);
-}
 
 //Näytetään kuva sivulla image-id:llä varustetussa elementissä
 function naytaKuva(kuva) {
     //console.log(kuva);
     $("#image").attr("src", kuva);
+    $("#image").show(300);
+
 }
 
 //haetaan fakta
 function haeFakta() {
-    var request = new XMLHttpRequest;
-    var url = "https://meowfacts.herokuapp.com/";
-
-    request.open("GET", url, true);
-    request.send();
-
-    request.onreadystatechange = function() {
-        if(request.readyState == 4 && request.status == 200) {
-            //console.log(request.responseText);
-            parseFakta(request.responseText);
-        } else if(request.readyState == 4 && request.status != 200) {
-            //Jos haku ei onnistu (haku on valmis, mutta status ei ole "OK"), kerrotaan käyttäjälle ja pyydetään suorittamaan uusi haku:
-            alert("Something went wrong while fetching a cat fact. Please try again!");
-        }
+    $.ajax({
+    url: "https://meowfacts.herokuapp.com/",
+    //jos haku onnistuu:
+    success: function(result) {
+        //console.log("haeFakta success " + result.data[0])
+        naytaFakta(result.data[0]);
+    },
+    //jos haku ei onnistu:
+    error: function (errorThrown) {
+        alert("There was a problem while fetching a cat fact. Please try again. Error: " + errorThrown);
+        console.log(errorThrown);
     }
+    });
 }
 
-//parsitaan, valitaan halutut tiedot
-function parseFakta(fakta) {
-    var jsonObjekti = JSON.parse(fakta);
-    var fakta = jsonObjekti.data[0];
+
+//Näytetään fakta faktapaikka-id:llä varustetussa elementissä
+function naytaFakta(fakta) {
+    //console.log("naytaFakta() " + fakta);
 
     //Api antaa muutaman tekstin, joita en halua näyttää. Niiden tullessa vastaan suoritetaan uusi haku
     if(fakta == "To unsubscribe from catfacts, reply the following code: tj3G5de$se"
     || fakta == "You gotta be kitten me! are you sure you want to unsubscribe? send YES or NO"
     || fakta == "Invalid Command, CatFacts!") {
-        //console.log("Nyt tuli tää: " + fakta)
+        console.log("Nyt tuli tää: " + fakta)
         haeFakta();
-    }
-    //console.log(fakta);
-    naytaFakta(fakta);
-}
+        }
 
-//Näytetään fakta faktapaikka-id:llä varustetussa elementissä
-function naytaFakta(fakta) {
     //jos childnodeja on, poistetaan ne (=edelliset haut)
-    $("#faktapaikka").empty();
+    $("#faktapaikka").empty().hide();
     //Lisätään fakta-paragrafi oikealle paikalle
     $("#faktapaikka").append("<p>" + fakta + "<p>");
-
-
-
+    $("#faktapaikka").show(800);
 }
